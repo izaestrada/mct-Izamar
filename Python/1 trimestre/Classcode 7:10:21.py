48e0c5fdc7b5814726cbebe8974a4a4be817f384
Izamar Estrada Loyo <104382069+izaestrada@users.noreply.github.com>
2022-05-31 09:45:25 -0500

    Add files via upload

---
+++ Python/1 trimestre/Classcode 7:10:21.py
@@ -0,0 +1,17 @@
+print('aprobados')
+
+
+print(5)
+
+
+mensaje='aprobados'
+mensaje2='reprobados'
+
+print(mensaje)
+print(mensaje2)
+
+mensaje='menos tarea para el 9 virtual'
+
+print(mensaje)
+
+print('este es mensaje guardado en memoria: ,')
