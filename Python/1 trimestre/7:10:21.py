c2c0e3e439d2890ca6620964c3c3b1001753cc12
Izamar Estrada Loyo <104382069+izaestrada@users.noreply.github.com>
2022-06-22 10:12:59 -0500

    Add files via upload

---
+++ Python/1 trimestre/7:10:21.py
@@ -0,0 +1,19 @@
+name='izamar'
+print('my name is ',name)
+
+grade='9D'
+print('iam from',grade )
+
+food='suchi'
+print('my favorite food is',food )
+
+print('adios mr')
+
+name='Valeria'
+print('my name is ',name)
+
+grade='9D'
+print('iam from',grade )
+
+food='tamales rancheros'
+print('my favorite food is',food )
