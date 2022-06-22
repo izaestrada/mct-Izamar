57cde946c9567499dca9522beac685f541c29999
Izamar Estrada Loyo <104382069+izaestrada@users.noreply.github.com>
2022-06-22 10:30:00 -0500

    Add files via upload

---
+++ Python/1 trimestre/Calculadora 18:11:21.py
@@ -0,0 +1,17 @@
+#suma
+number=int(input('ingrese su numero: '))
+number2=int(input('ingresa otro numero: '))
+
+print('la suma de estos numeros es:', (number+number2))
+
+#resta
+number=int(input('ingrese su numero:'))
+number2=int(input('ingrese otro numero:'))
+
+print('la resta de estos numeros es:',(number-number2))
+
+#multipicacion
+number=int(input('ingrese su numero:'))
+number2=int(input('ingrese otro numero:'))
+
+print('el resultado es:',(number*number2))
---
+++ Python/1 trimestre/Input 18:11:21.py
@@ -0,0 +1,15 @@
+
+nombre=input('ingrese su nombre')
+print('saludos,',nombre)
+
+pais=input('de que pais sos?')
+print(pais,'es bonito ')
+
+
+print(nombre,'es de', pais,'y es buena onda')
+
+
+numero=int(input('ingrese numero:'))
+numero2=int(input('ingrese numero:'))
+
+print('la suma de estos numeros es:',(numero+numero2))
