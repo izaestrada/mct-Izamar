6aefa74dd8d88a326af7a21933d55b0c9cad13c3
Izamar Estrada Loyo <104382069+izaestrada@users.noreply.github.com>
2022-06-22 10:15:43 -0500

    Add files via upload

---
+++ Python/1 trimestre/14:10:21.py
@@ -0,0 +1,23 @@
+trombones76= 'big grade'
+more=10000
+classe='hello there'
+
+
+
+alpha=2
+b3t4=6
+
+suma=alpha+b3t4
+print('la suma jue: ', suma)
+
+resta=alpha-b3t4
+print('la resta fue: ', resta)
+
+multi=alpha*b3t4
+print('multiplicacion: ', multi)
+
+div=alpha/b3t4
+print('division:', div)
+
+potencia=alpha**b3t4
+print('potencia:', potencia)
