389f02d57378a59170feb0b2afb6f7bf68cf2c72
Izamar Estrada Loyo <104382069+izaestrada@users.noreply.github.com>
2022-06-22 10:22:39 -0500

    Add files via upload

---
+++ Python/1 trimestre/21:10:21.py
@@ -0,0 +1,4 @@
+print("how can we act with empathy and positivity when were online")
+print('be always positive ')
+
+print('identity,the boy that write the letter,the girl and the classmates ')
---
+++ Python/1 trimestre/Review 4:11:21.py
@@ -0,0 +1,13 @@
+saludo='hola'
+print(saludo)
+
+print('ingrese su saludo')
+saludo=input()
+print('escribe tu',saludo)
+
+print('escribe tu nombre')
+nombre=input()
+print('tu nombre es:',nombre)
+
+apellido=input('escribe tu apellido: ')
+print('tu apellido es', apellido)
