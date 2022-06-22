ba675b9dd76c2254d7535494b5d7aecbf451ada1
Izamar Estrada Loyo <104382069+izaestrada@users.noreply.github.com>
2022-06-22 10:26:25 -0500

    Add files via upload

---
+++ Python/1 trimestre/Code for the week 4:11:21.py
@@ -0,0 +1,20 @@
+saludo='hiii'
+print(saludo)
+
+print('what is your name?')
+name=input()
+print('nice name',name)
+
+
+print('how old are you?')
+age=input()
+
+print('where are you from?')
+place=input()
+print('i love',place)
+
+print('what things do you like to do?')
+ldk=input()
+
+print('what is you favorite movie?')
+movie=input()
---
+++ Python/1 trimestre/Input 4:11:21.py
@@ -0,0 +1,11 @@
+saludo=('hi')
+print(saludo)
+
+print('what is your name?')
+name=input()
+
+print('in which group are you?')
+group=input()
+
+print('what is your favorite food?')
+food=input()
