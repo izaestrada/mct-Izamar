1ff8a01f9dc8b2a44e3ddc4f04d7daf8574d7738
Izamar Estrada Loyo <104382069+izaestrada@users.noreply.github.com>
2022-06-23 13:57:54 -0500

    Add files via upload

---
+++ Python/3er parcial/8:02:22.py
@@ -0,0 +1,14 @@
+while True:
+ numero=int(input("enter the velocity:"))
+
+ if(numero>25):
+  print("alerta critica")
+
+ elif (numero==20):
+  print("alerta de un rayo en el cielo")
+
+ elif (numero>10):
+   print("estrella fugaz")
+
+ else:
+   print ("tu que")
